840de20f5c72ce602fd77239cf871f8f539b5e09
willmaier <wmaier4210@gmail.com>
2024-08-22 13:55:52 -0400

    Init - added globals and missing scenes

--- scenes/globals.gd
+++ scenes/globals.gd
@@ -1,3 +1,22 @@
 extends Node
 
+# signals 
+signal enemy_attack
 
+# player variables
+# var player_ui: Control
+
+# fight scene varaiables
+var player_turn: bool = true
+
+func fightStart():
+	# player_ui = PlayerUI
+	pass
+
+func enemyTurn():
+	#enemy_attack.emit()
+	player_turn = false
+	print("enemy turn")
+
+func attackEnemy(damage):
+	pass
--- scenes/player_fighter/player_fighter.gd
+++ scenes/player_fighter/player_fighter.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 # this will change based on gear
-@export var player_health: int
+@export var player_health: int = 10
 @export var player_max_health: int = 10
 @export var player_fuel: int = 10
 @export var player_max_fuel: int = 10
