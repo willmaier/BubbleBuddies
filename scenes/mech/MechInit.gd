4981d7c57470bae421721f2b161d74d723c48f11
Don Prolad <dghdp37@gmail.com>
2024-08-28 22:45:52 +0200

    feature/ui intergration

---
+++ scenes/mech/MechInit.gd
@@ -0,0 +1,15 @@
+extends ResourcePreloader
+class_name MechInit
+
+#@onready var mech_helmet : Texture = preload("res://assets/Art/GEAR/HELMET/helmet_base.png")
+#@onready var mech_feet : Texture = preload("res://assets/Art/GEAR/FOOT/foot_base.png")
+#@onready var mech_torso : Texture = preload("res://assets/Art/GEAR/TORSO/torso_base.png")
+#@onready var mech_wing : Texture = preload("res://assets/Art/GEAR/WING/wing_base.png")
+#
+##HEALTH
+#@onready var max_health : int = 10
+#
+##FUEL
+#@onready var max_fuel : int = 10
+
+
